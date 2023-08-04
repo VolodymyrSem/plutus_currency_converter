e8667bd1a43603fc3a349ac297eec1dfd59232dc
VolodymyrSem <semenchuk.volodymyr@gmail.com>
2023-08-04 17:40:41 +0200

    Added a new method get_rate_from_API to get rate with inputted owned currency and target currency right away

--- converter/handlers/API.py
+++ converter/handlers/API.py
@@ -10,8 +10,11 @@ class APIHandler:
     def __check_response(self, response):
         if response.status_code != 200:
             raise ServerError
+    def get_rate_from_API(self, from_currency: str, to_currency: str) -> float:
+        rates = self.get_rates_from_API(from_currency)
+        return rates.get(to_currency)
 
-    def get_rates_from_api(self, currency_code: str = 'USD') -> dict:
+    def get_rates_from_API(self, currency_code: str = 'USD') -> dict:
         response = self.__go_to_API(currency_code).json()
         return response.get('rates')
 
@@ -20,9 +23,14 @@ class APIHandler:
         return list(response.get('rates').keys())
 
     def __go_to_API(self, currency_code: str = 'USD') -> requests.Response:
-        return requests.get(
+        self.set_session()
+        return self.session.get(
             self.__get_link_to_API(currency_code)
         )
+
+    def set_session(self):
+        if not hasattr(self, 'session'):
+            setattr(self, 'session', requests.Session())
 
     def __get_link_to_API(self, currency_code: str = 'USD') -> str:
         return self.__API_URL + currency_code.upper()
